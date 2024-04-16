c4fc48e5479d77b240ee556e40650ddaa1cd2990
Nešad Omeragić <nesad.omeragich27@gmail.com>
2024-04-16 11:50:01 +0200

    Add sub items, add HeaderToImage converter

---
+++ Converters/HeaderToImageConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace WPF.Workshop
+{
+    public class HeaderToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var path = value as string;
+
+            if (string.IsNullOrEmpty(path))
+                return null;
+
+            return new BitmapImage(new Uri("pack://application:,,,/Images/drive.png"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -20,8 +23,80 @@ namespace WPF.Workshop
             {
                 var treeViewItem = new TreeViewItem();
                 treeViewItem.Header = driveName;
+                treeViewItem.Tag = driveName;
+
+                treeViewItem.Items.Add(null);
+
+                treeViewItem.Expanded += Folder_Expanded;
+
+
                 FolderView.Items.Add(treeViewItem);
             }
+        }
+
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            var item = sender as TreeViewItem;
+
+            if (item.Items.Count != 1 || item.Items[0] != null)
+                return;
+
+            item.Items.Clear();
+
+            var fullPath = item.Tag as string;
+            var directories = new List<string>();
+            var files = new List<string>();
+
+            try
+            {
+                var dirs = Directory.GetDirectories(fullPath);
+                var fs = Directory.GetFiles(fullPath);
+                
+                if (dirs.Length > 0)
+                    directories.AddRange(dirs);
+
+                if (fs.Length > 0)
+                    files.AddRange(fs);
+            }
+            catch
+            {
+                //Ignore
+            }
+
+            foreach (var directoryPath in directories) 
+            {
+                var newItem = new TreeViewItem();
+                newItem.Header = GetFileFolderName(directoryPath);
+                newItem.Tag = directoryPath;
+                newItem.Items.Add(null); 
+                newItem.Expanded += Folder_Expanded;
+
+                item.Items.Add(newItem);
+            }
+
+            foreach (var filesPath in files)
+            {
+                var newItem = new TreeViewItem();
+                newItem.Header = GetFileFolderName(filesPath);
+                newItem.Tag = filesPath;
+                newItem.Expanded += Folder_Expanded;
+
+                item.Items.Add(newItem);
+            }
+        }
+
+        public static string GetFileFolderName(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                return string.Empty;
+
+            var normalizedPath = path.Replace('/', '\\');
+            var lastIndex = normalizedPath.LastIndexOf('\\');
+
+            if (lastIndex <= 0)
+                return path;
+
+            return normalizedPath.Substring(lastIndex + 1);
         }
     }
 }
