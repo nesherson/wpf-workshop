e7c9bceedd7eb047a28b854ed76a5b839d5052cc
Nešad Omeragić <nesad.omeragich27@gmail.com>
2024-04-18 23:56:18 +0200

    Add logo, title and window buttons

--- WPF.Tesetto.Word/ViewModels/Window/WindowViewModel.cs
+++ WPF.Tesetto.Word/ViewModels/Window/WindowViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using PropertyChanged;
+using System;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace WPF.Tesetto.Word
 {
@@ -13,22 +16,20 @@ namespace WPF.Tesetto.Word
         {
             _window = window;
 
+            OuterMarginSize = 10;
+
+            MinimizeCommand = new RelayCommand(Minimize);
+            MaximizeCommand = new RelayCommand(Maximize);
+            CloseCommand = new RelayCommand(Close);
+            MenuCommand = new RelayCommand(Menu);
+
             _window.StateChanged += Window_StateChanged;
         }
-
-        private void Window_StateChanged(object? sender, System.EventArgs e)
-        {
-            OnPropertyChanged(nameof(ResizeBorderThickness));
-            OnPropertyChanged(nameof(OuterMarginSize));
-            OnPropertyChanged(nameof(OuterMarginSizeThickness));
-            OnPropertyChanged(nameof(WindowRadius));
-            OnPropertyChanged(nameof(WindowCornerRadius));
-        }
-
+   
         public int ResizeBorder { get; set; } = 6;
 
-        public int OuterMarginSize 
-        { 
+        public int OuterMarginSize
+        {
             get
             {
                 return _window.WindowState == WindowState.Maximized ? 0 : _outerMarginSize;
@@ -57,5 +58,49 @@ namespace WPF.Tesetto.Word
 
         public CornerRadius WindowCornerRadius => new(WindowRadius);
 
+        public int TitleHeight { get; set; } = 40;
+
+        public GridLength TitleHeightGridLength => new(TitleHeight + ResizeBorder);
+
+        public ICommand MenuCommand { get; set; }
+        public ICommand MinimizeCommand { get; set; }
+        public ICommand MaximizeCommand { get; set; }
+        public ICommand CloseCommand { get; set; }
+
+        private void Window_StateChanged(object? sender, System.EventArgs e)
+        {
+            OnPropertyChanged(nameof(ResizeBorderThickness));
+            OnPropertyChanged(nameof(OuterMarginSize));
+            OnPropertyChanged(nameof(OuterMarginSizeThickness));
+            OnPropertyChanged(nameof(WindowRadius));
+            OnPropertyChanged(nameof(WindowCornerRadius));
+        }
+
+        private void Minimize()
+        {
+            _window.WindowState = WindowState.Minimized;
+        }
+
+        private void Maximize()
+        {
+            _window.WindowState ^= WindowState.Maximized;
+        }
+
+        private void Close()
+        {
+            _window.Close();
+        }
+
+        private void Menu()
+        {
+            SystemCommands.ShowSystemMenu(_window, GetMousePosition());
+        }
+
+        private Point GetMousePosition()
+        {
+            var position = Mouse.GetPosition(_window);
+
+            return new Point(position.X + _window.Left, position.Y + _window.Top);
+        }
     }
 }
