fcfa88a7bd2bb9a0059d09e3ddb5db6da29ba7b4
Nešad Omeragić <nesad.omeragich27@gmail.com>
2024-04-20 01:02:22 +0200

    Add BaseValueConverter

---
+++ WPF.Tesetto.Word/ValueConverters/BaseValueConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace WPF.Tesetto.Word.ValueConverters
+{
+    public abstract class BaseValueConverter<T> : MarkupExtension, IValueConverter
+        where T : class, new()
+    {
+        private static T _converter = null;
+
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+
+        public abstract object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture);
+
+        public override object ProvideValue(IServiceProvider serviceProvider) => _converter ?? (_converter = new T());
+
+    }
+}
