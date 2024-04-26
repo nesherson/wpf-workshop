4bc28bb89479b16d4393a9634e05435a0b8acb90
Nešad Omeragić <nesad.omeragich27@gmail.com>
2024-04-26 23:45:09 +0200

    Add BaseAttachedProperty - WIP

---
+++ WPF.Tesetto.Word/AttachedProperties/BaseAttachedProperty.cs
@@ -0,0 +1,8 @@
+﻿namespace WPF.Tesetto.Word
+{
+    public abstract class BaseAttachedProperty<TParent, TProperty>
+        where TParent : new()
+    {
+        public static TParent Instance { get; private set; } = new TParent(); 
+     }
+}
