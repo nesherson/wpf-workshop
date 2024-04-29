c7456d6879f4618a4a172fb189fe70d7e1f86b5e
Nešad Omeragić <nesad.omeragich27@gmail.com>
2024-04-29 23:52:21 +0200

    Add functionality for BasePage to use generic view model

--- WPF.Tesetto.Word/Pages/BasePage.cs
+++ WPF.Tesetto.Word/Pages/BasePage.cs
@@ -6,13 +6,17 @@ using System.Windows.Media.Animation;
 
 namespace WPF.Tesetto.Word
 {
-    public class BasePage : Page
+    public class BasePage<TViewModel> : Page
+        where TViewModel : BaseViewModel, new()
     {
+        private TViewModel _viewModel;
+
         public BasePage()
         {
             if (PageLoadAnimation != PageAnimation.None)
                 Visibility = Visibility.Collapsed;
 
+            ViewModel = new TViewModel();
 
             Loaded += BasePage_Loaded;
         }
@@ -20,6 +24,19 @@ namespace WPF.Tesetto.Word
         public PageAnimation PageLoadAnimation { get; set; } = PageAnimation.SlideAndFadeInFromRight;
         public PageAnimation PageUnloadAnimation { get; set; } = PageAnimation.SlideAndFadeOutToLeft;
         public float SlideSeconds { get; set; } = 0.8f;
+
+        public TViewModel ViewModel
+        {
+            get => _viewModel;
+            set
+            {
+                if (_viewModel == value)
+                    return;
+
+                _viewModel = value;
+                DataContext = _viewModel;
+            }
+        }
 
         private async void BasePage_Loaded(object sender, System.Windows.RoutedEventArgs e)
         {
