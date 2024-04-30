d1767f469118f2240ed3baeb8f2e5c9366297b96
Nešad Omeragić <nesad.omeragich27@gmail.com>
2024-04-30 23:58:26 +0200

    Add LoginViewModel

---
+++ WPF.Tesetto.Word/ViewModels/Window/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PropertyChanged;
+
+namespace WPF.Tesetto.Word
+{
+	[AddINotifyPropertyChangedInterface]
+    public class LoginViewModel : BaseViewModel
+    {
+        public LoginViewModel()
+        {
+        }
+    }
+}
