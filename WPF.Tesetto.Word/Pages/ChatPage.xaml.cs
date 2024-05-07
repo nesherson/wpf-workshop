9937ce989db9e1f55b3f2e4a270df6a3042e96d8
Nešad Omeragić <nesad.omeragich27@gmail.com>
2024-05-07 23:55:59 +0200

    Chat page initial structure

---
+++ WPF.Tesetto.Word/Pages/ChatPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WPF.Tesetto.Word
+{
+	/// <summary>
+	/// Interaction logic for ChatPage.xaml
+	/// </summary>
+	public partial class ChatPage : Page
+	{
+		public ChatPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
